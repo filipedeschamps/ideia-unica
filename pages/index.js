89afefcc6103a0bee320ced4efb5907cb7825819
Filipe Deschamps <filiped@gmail.com>
2020-11-19 11:17:44 -0800

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
-    return <h1>Home</h1>
+    return (<div>
+        <h1>Home</h1>
+
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (<div>
+        <h1>Sobre</h1>
+
+        <Link href="/">
+            <a>Acessar página Home</a>
+        </Link>
+    </div>)
+}
+
+export default Sobre
