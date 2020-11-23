27fcc66200014465edf577035d3813bb2b3f1306
Filipe Deschamps <filiped@gmail.com>
2020-11-23 10:39:01 -0800

    adiciona logs

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,4 +1,5 @@
 function Tempo(props) {
+    console.log('> Passando pelo Frontend;')
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
@@ -11,6 +12,7 @@ function Tempo(props) {
 }
 
 export function getStaticProps() {
+    console.log('> Passando pelo getStaticProps();')
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
