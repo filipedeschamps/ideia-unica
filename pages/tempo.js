065d6fbada42589c303904c89d6940728c9b384e
Filipe Deschamps <filiped@gmail.com>
2020-11-23 10:37:06 -0800

    test getStaticProps

---
+++ pages/tempo.js
@@ -0,0 +1,24 @@
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
+        </div>
+    )
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+
+export default Tempo;
