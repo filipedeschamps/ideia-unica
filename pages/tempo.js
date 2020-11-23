c2e88ce75c1a7c0d48cb5b3892911c55736d0eee
Filipe Deschamps <filiped@gmail.com>
2020-11-23 15:10:23 -0800

    adiciona revalidade

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,26 +6,22 @@ function Tempo(props) {
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático - com delay)</div>
+            <div>{props.staticDateString} (estático)</div>
         </div>
     )
 }
 
 export async function getStaticProps() {
     console.log('> Passando pelo getStaticProps();');
-    console.log('> Adicionando delay de 5 segundos');
-
-    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 1
     }
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
