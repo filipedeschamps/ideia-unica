aaf589d296324ff7508e0072b52bca95338c0b61
Filipe Deschamps <filiped@gmail.com>
2020-11-23 14:46:07 -0800

    adicionando delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,13 +6,16 @@ function Tempo(props) {
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
         </div>
     )
 }
 
-export function getStaticProps() {
-    console.log('> Passando pelo getStaticProps();')
+export async function getStaticProps() {
+    console.log('> Passando pelo getStaticProps();');
+    console.log('> Adicionando delay de 5 segundos');
+
+    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
@@ -22,5 +25,7 @@ export function getStaticProps() {
         }
     }
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
