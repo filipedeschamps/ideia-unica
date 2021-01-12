585a769f52e04ba0254f38b50f46cd4a1cbb7328
Filipe Deschamps <filiped@gmail.com>
2021-01-12 15:43:06 -0800

    usa variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     });
 }
 
